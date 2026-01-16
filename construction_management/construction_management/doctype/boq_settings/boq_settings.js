7b536f5413c98048373ccd6fe7c3a85fe397789c
Samarth Upare <samarth@dieture.com>
2026-01-16 21:30:00 +0530

    feat(boq_settings): Implement account query filtering based on selected company

--- construction_management/construction_management/doctype/boq_settings/boq_settings.js
+++ construction_management/construction_management/doctype/boq_settings/boq_settings.js
@@ -1,8 +1,40 @@
 // Copyright (c) 2026, Construction Management and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("BOQ Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("BOQ Settings", {
+	onload: function (frm) {
+		set_account_queries(frm);
+	},
+	refresh: function (frm) {
+		set_account_queries(frm);
+	},
+	company: function (frm) {
+		set_account_queries(frm);
+	}
+});
 
-// 	},
-// });
+function set_account_queries(frm) {
+	if (!frm.doc.company) return;
+
+	// List of account link fields to filter by company
+	const account_fields = [
+		"retention_account",
+		"advance_account",
+		"asset_labor_cost_account",
+		"asset_cost_account",
+		"expenses_account",
+		"overhead_account",
+		"salary_labor_account",
+		"asset_cost_credit"
+	];
+
+	account_fields.forEach((fieldname) => {
+		frm.set_query(fieldname, function () {
+			return {
+				filters: {
+					company: frm.doc.company
+				}
+			};
+		});
+	});
+}
