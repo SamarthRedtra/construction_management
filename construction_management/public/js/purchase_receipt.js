f40a8f7b4b36d20d42fb3e0dd513e7962bb804a6
Samarth Upare <samarth@dieture.com>
2026-02-16 19:37:12 +0530

    fixes

--- construction_management/public/js/purchase_receipt.js
+++ construction_management/public/js/purchase_receipt.js
@@ -45,6 +45,29 @@ frappe.ui.form.on('Purchase Receipt', {
 			construction_management.dimension_utils.setup_accounting_dimension_filters(frm);
 			construction_management.dimension_utils.setup_child_table_dimension_filters(frm, 'items');
 		}
+
+		// Explicit queries for BOQ dimensions in child table
+		frm.set_query("bill_no", "items", function (doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			let project = row.project || doc.project;
+			if (project) {
+				return { filters: { project: project } };
+			}
+			return {};
+		});
+
+		frm.set_query("boq_item", "items", function (doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			if (row.bill_no) {
+				return { filters: { parent_bill: row.bill_no } };
+			} else {
+				let project = row.project || doc.project;
+				if (project) {
+					return { filters: { project: project } };
+				}
+			}
+			return {};
+		});
 	}
 });
 
