8b2f045da38c1f5ff97ae8f7d72c4489de16c4fc
Samarth Upare <samarth@dieture.com>
2026-06-18 19:21:23 +0530

    feat: add logic to validate frozen or disabled parties for current asset accounts when enabled in custom settings

--- construction_management/overrides/gl_entry.py
+++ construction_management/overrides/gl_entry.py
@@ -17,6 +17,20 @@ class GLEntryOverride(GLEntry):
 					self.party_type, self.party, self.company, self.account_currency
 				)
 			return
+
+		import frappe
+		allow_party = False
+		try:
+			allow_party = frappe.db.get_single_value("Redtra Custom Setting", "allow_party_on_current_asset")
+		except Exception:
+			pass
+
+		if allow_party and self.party_type and self.party:
+			account_type = frappe.get_cached_value("Account", self.account, "account_type")
+			if account_type == "Current Asset":
+				validate_party_frozen_disabled(self.company, self.party_type, self.party)
+				return
+
 		super().validate_party()
 
 	def _is_pr_extra_accounting_entry(self):
